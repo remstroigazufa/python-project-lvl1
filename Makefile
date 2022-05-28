699e0df71bc1e2776033d2521431581002ae020e
Соколов Артем <remstroigazufa@mail.ru>
2022-05-28 19:31:52 +0500

    workflows

--- Makefile
+++ Makefile
@@ -9,5 +9,9 @@ publish:
 package-install:
 	python3 -m pip install --user dist/*.whl  --force-reinstall
 
+lint:
+	poetry run flake8 brain_games
+
+
 .PHONY: brain-games
 
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,16 @@
-#!/usr/bin/env python3
+'#!/usr/bin/env python3'
+import prompt
+
+
 def main():
     print('Welcome to the Brain Games!')
+    welcome_user()
+
+
+def welcome_user():
+    name = prompt.string('May I have your name? ')
+    print('Hello, ', name, '!')
+
+
 if __name__ == '__main__':
     main()
-
