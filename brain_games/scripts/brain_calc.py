308dc8a3f4cccec75d0b42cb426613ca75472034
Соколов Артем <remstroigazufa@mail.ru>
2022-06-05 16:45:47 +0500

    brain-calc

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,38 @@
+'#!/usr/bin/env python3'
+import prompt
+import random
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print('Hello, ', name,'!')
+    print('What is the result of the expression?')
+    a = 1
+    while a < 4:
+        i = random.randrange(1, 100, 1)
+        y = random.randrange(1, 100, 1)
+#        x = '+'
+#        v = '-'
+#        s = '*' 
+        j = ['+','-','*']
+        operation = random.choice(j)
+        print('Question: ',i,operation, y )
+        b = prompt.integer('Your answer: ' )
+        g = eval(f'i {operation} y')
+#        print('g = ',g,'b=',b)
+#        print('g -',type(g),'b -',type(b))
+#        print('g == b' ,g==b)
+        if a < 4 and g == b:
+            print('Correct!')
+            a = a + 1
+        else:
+            print(b,' is wrong answer ;(. Correct answer was ', g,'.\nLet\'s try again, ', name,"!")
+            break
+        if a == 4:
+            print('Congratulations, ',name,'!')
+            break
+
+
+if __name__ == '__main__':
+    main()
