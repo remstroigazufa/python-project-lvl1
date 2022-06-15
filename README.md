29d5426dee4b925ffbcdb9c2c2f9aa8729838322
Соколов Артем <remstroigazufa@mail.ru>
2022-06-15 20:29:28 +0500

    python_project

--- README.md
+++ README.md
@@ -4,4 +4,16 @@
 [![make_lint](https://github.com/remstroigazufa/python-project-lvl1/actions/workflows/make_lint.yml/badge.svg)](https://github.com/remstroigazufa/python-project-lvl1/actions/workflows/make_lint.yml)
 
 
- https://asciinema.org/a/CXxWnXJ8P8ZxzesUdlR3tUZio
+https://asciinema.org/a/CXxWnXJ8P8ZxzesUdlR3tUZio
+
+
+https://asciinema.org/a/0tQzTfoFFcPZwstjp3GxAjtwZ
+
+
+https://asciinema.org/a/30BrklNCd2RSKWPsgOf4sEMHa
+
+
+https://asciinema.org/a/F0PsGo2FWYuot2elGzmYlual3
+
+
+https://asciinema.org/a/OzKeqw4f8cIeDjepYo3LN71z7
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,29 +1,27 @@
 '#!/usr/bin/env python3'
 import prompt
 import random
+import math
 
 
 def main():
     print('Welcome to the Brain Games!')
-    main_three()
-
-
-def main_three():
     name = prompt.string('May I have your name? ')
     print('Hello,', name, '!')
-    print('Answer "yes" if the number is even, otherwise answer "no"')
+    print('Find the greatest common divisor of given numbers.')
     a = 1
     while a < 4:
         i = random.randrange(1, 100, 1)
-        print('Question:', i)
-        b = input('Your answer:')
-        cor = 'yes' if i % 2 == 0 else 'no'
-        if cor == b:
+        y = random.randrange(1, 100, 1)
+        print('Question:', i, y)
+        b = prompt.integer('Your answer:')
+        g = math.gcd(i, y)
+        if a < 4 and g == b:
             print('Correct!')
             a = a + 1
         else:
-            print(f'\'{b}\' is wrong answer ;(. Correct answer was\'{cor}\'')
-            print('Let\'s try again, {name}!')
+            print(b, 'is wrong answer ;(. Correct answer was', g, '.')
+            print('Let\'s try again,', name, "!")
             break
         if a == 4:
             print('Congratulations,', name, '!')
