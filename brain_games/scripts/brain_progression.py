eedb3b1e066c2489be1acc4a1dee62bc4b3182d4
Соколов Артем <remstroigazufa@mail.ru>
2022-06-15 20:39:10 +0500

    python_project

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -6,34 +6,29 @@ import random
 def main():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
-    print('Hello, ', name, '!')
+    print('Hello, ', name,'!')
     print('What number is missing in the progression?')
     a = 1
     while a < 4:
-        i = random.randrange(1, 9, 1)
-        y = random.randrange(70, 100, 1)
-        x = random.randrange(1, 5, 1)
-        u = random.randrange(1, 9, 1)
-        r = list(range(i, y, x))
-        g = r[0:9]
-        q = g[u]
-        g[u] = '..'
-        r = ' '
-        for k in range(len(g)):
-            r += f'{g[k]} '
-        print('Question: ', r)
-        b = prompt.integer('Your answer: ')
-        if a < 4 and q == b:
+        i = random.randrange(1, 10, 1)
+        y = random.randrange(30, 100, 1)
+        x = random.randrange(1, 10, 1)
+        g = list(range(i, y, x))
+        for n in str(g) :
+            str(n)+=g(i)
+        print('Question: ',n)
+        b = prompt.integer('Your answer: ' )
+        if a < 4 and g == b:
             print('Correct!')
             a = a + 1
         else:
-            print(b, 'is wrong answer ;(. Correct answer was', q)
-            print('Let\'s try again, ', name, "!")
+            print(b,' is wrong answer ;(. Correct answer was ', g,'.\nLet\'s try again, ', name,"!")
             break
         if a == 4:
-            print('Congratulations, ', name, '!')
+            print('Congratulations, ',name,'!')
             break
 
 
 if __name__ == '__main__':
     main()
+
