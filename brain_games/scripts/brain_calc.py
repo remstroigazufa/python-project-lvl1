226c74f700829f75c50076a0ee254a60c2155666
Соколов Артем <remstroigazufa@mail.ru>
2022-06-19 15:34:10 +0500

    brain-games

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -22,10 +22,10 @@ def main():
             a = a + 1
         else:
             print(b, 'is wrong answer ;(. Correct answer was', g, '.')
-            print('Let\'s try again, ', name, "!")
+            print('Let\'s try again,', name + "!")
             break
         if a == 4:
-            print('Congratulations,', name, '!')
+            print('Congratulations,', name + '!')
             break
 
 
